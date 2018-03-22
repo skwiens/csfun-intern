62eeb40460006c97510d47ceb2d6b64937cff9da
Shaunna <skwiens@gmail.com>
2018-03-21 23:04:58 -0700

    string update

--- ruby/strings.rb
+++ ruby/strings.rb
@@ -2,7 +2,6 @@
 def reverse_in_place(string)
   i = 0
   j = string.length - 1
-
   while i < j
     temp = string[i]
     string[i] = string[j]
@@ -10,6 +9,7 @@ def reverse_in_place(string)
     i += 1
     j -= 1
   end
+  return string
 end
 # Time Complexity: O(n)
 # Space Complexity: O(1)
@@ -36,6 +36,7 @@ def reverse_words_new_sentence(sentence)
   return sentence_array.join(' ')
 end
 
+
 def reverse_words_in_place(sentence)
   puts sentence.length
   i = 0
@@ -57,14 +58,71 @@ def reverse_words_in_place(sentence)
     end
 
     i = j
-    puts sentence
-    puts i
-    puts j
   end
 
   return sentence
 end
 
+# Design and implement a method to reverse the words in a sentence, in place.
+def reverse_sentence(sentence)
+  return reverse_words_in_place(reverse_in_place(sentence))
+end
+
+# Design and implement a method to check if the input string is a palindrome. Return true if the string is a palindrome. Return false otherwise.
+def palindrome?(string)
+  i = 0
+  j = string.length - 1
+
+  while i < j
+    while string[i] == " "
+      i += 1
+    end
+    while string[j] == " "
+      j -= 1
+    end
+    if string[i].downcase != string[j].downcase
+      return false
+    else
+      i += 1
+      j -= 1
+    end
+  end
+
+  return true
+end
+
+
+# Design and implement a method that updates the string by replacing consecutive repeating characters with a number representing the frequncy. The replacement is done only if the string length will get reduced by the process. In addition to .length, you may use .slice! to implement this method.
+
+# e.g "aaabbbbbcccc" will get replace to "a3b5c4", since a is repeated 3 times, b is repeated 5 times and c is repeated 4 times
+# "xxxyttttgeee" will get replaced with "x3yt4ge3". 'y' and 'g' do not get replaced by "y1" and "g1" since that will increase the length.
+# "ddbbfffgjjjj" will get replaced with "ddbbf3gj4". "dd" and "bb" do not get replaced since the length will remain the same with "d1" and "b1". "g" does not get replaced with "g1" since that will increase the length.
+
+def update(string)
+  compare = 0
+  current = 1
+  count = 1
+
+  while compare < string.length
+    while string[current] == string[compare]
+      count += 1
+      current += 1
+      if count > 1
+        string.slice!(current)
+      end
+    end
+
+    if count > 1
+      string[compare + 1] = count.to_s
+    end
+
+    compare = current
+    current += 1
+    count = 1
+  end
+
+  return string
+end
 
 
 
@@ -73,4 +131,4 @@ end
 # puts(reverse_in_place('Ada Lovelace'))
 # puts(reverse_new_string('Ada Lovelace'))
 # puts(reverse_words_new_sentence('I love pizza'))
-puts(reverse_words_in_place('I love pizza'))
+puts(update('aaabbbbbcccc'))
